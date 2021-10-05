5d490f57a70cab10ba2f23f6cea461cdd1a68d21
macross0 <maxnekuga@gmail.com>
2021-10-05 23:54:44 +0300

    Gallery init

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ function change_section(section, animate) {
 			appear.style.position = 'relative';
 			appear.style.left = '0';
 			appear.style.opacity = '1';
-			
 		}
 		else {
 			$(sections[i]).show();
@@ -49,6 +48,35 @@ function change_section(section, animate) {
 // reset transition time after changing section or language
 function changeTransition(sender) {
 	sender.style.transition = "0.3s";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
 
 $(document).ready(function() {
