6c22de974e1515ea825362fc77ec15ff52e7f102
macross0 <maxnekuga@gmail.com>
2021-10-06 11:40:40 +0300

    Finished images gallery

--- script.js
+++ script.js
@@ -50,35 +50,6 @@ function changeTransition(sender) {
 	sender.style.transition = "0.3s";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
 $(document).ready(function() {
 
 	//var navbar_selection = document.getElementById("current_navbar_selection");
